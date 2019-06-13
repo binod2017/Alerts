ed3157fba19f157307f51dac99e6786eedbb58a7
binod2017 <binodk3@gmail.com>
2019-06-13 22:00:13 +0530

    Add project files.

---
+++ Alerts/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Alerts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbName = new System.Windows.Forms.ComboBox();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtOthers = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alert On";
+            // 
+            // cmbName
+            // 
+            this.cmbName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cmbName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbName.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmbName.FormattingEnabled = true;
+            this.cmbName.Location = new System.Drawing.Point(68, 29);
+            this.cmbName.Name = "cmbName";
+            this.cmbName.Size = new System.Drawing.Size(240, 21);
+            this.cmbName.TabIndex = 4;
+            this.cmbName.SelectedIndexChanged += new System.EventHandler(this.cmbName_SelectedIndexChanged);
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTime.Location = new System.Drawing.Point(405, 29);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.Size = new System.Drawing.Size(190, 20);
+            this.dtpTime.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 498);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(352, 83);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(26, 83);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(123, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(515, 83);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(123, 23);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtOthers
+            // 
+            this.txtOthers.Location = new System.Drawing.Point(68, 56);
+            this.txtOthers.Name = "txtOthers";
+            this.txtOthers.Size = new System.Drawing.Size(527, 20);
+            this.txtOthers.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 624);
+            this.Controls.Add(this.txtOthers);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.cmbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbName;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtOthers;
+    }
+}
+
---
+++ Alerts/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alerts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Alerts";
+            txtOthers.Enabled = false;
+        }
+
+        string[] allfiles = new string[]
+            {
+                "AlertData.xlsx"
+            };
+
+        public string AlertConnection
+        {
+            get
+            {
+                return ConfigurationManager
+                    .ConnectionStrings["AlertDBConnection"]
+                    .ToString();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Close the Expense form
+            bool saveittogoogle = Utilities.SaveItToGoogleDrive(allfiles);
+            if (saveittogoogle)
+            {
+                //MessageBox.Show("Updated in Google Drive");
+            }
+            else { MessageBox.Show("Error in saving"); }
+            bool saveittoDbBackup = Utilities.SaveItToDbBackup(allfiles);
+            if (saveittoDbBackup)
+            {
+                //MessageBox.Show("Updated in Local Backup");
+            }
+            else { MessageBox.Show("Error in saving"); }
+            this.Close();
+        }
+        private int id;
+        private string rawname;
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DataTable data = GetLastIdInt();
+            {
+                id = int.Parse(data.Rows[0]["Expr1000"].ToString()) + 1;
+            }
+            if (cmbName.SelectedItem.ToString() =="Others")
+            {
+                rawname = txtOthers.Text;
+            }
+            else
+            {
+                rawname = cmbName.SelectedItem.ToString();
+            }
+            string name = rawname.Replace(' ', '_');
+            string alertTime = dtpTime.Value.AddDays(-1).ToShortDateString();
+            string alertMonth = Utilities.ConvertIntToMonth(dtpTime.Value.Month);
+            string alertYear = DateTime.Now.Year.ToString();
+            string created = DateTime.Now.ToShortDateString();
+            int rowsAffected = 0;
+            using (OleDbCommand oleDbCommand = new OleDbCommand())
+            {
+                // Set the command object properties
+                oleDbCommand.Connection = new OleDbConnection(this.AlertConnection);
+                oleDbCommand.CommandType = CommandType.Text;
+                oleDbCommand.CommandText = "Insert Into" +
+                " [AlertData$] (Id, Name, AlertTime, AlertMonth, AlertYear, Created)" +
+                " Values (@id, @name, @alertTime, @alertMonth, @alertYear, @created)";// Scripts.sqlInsertExpense;
+
+                // Add the input parameters to the parameter collection
+                oleDbCommand.Parameters.AddWithValue("@id", id);
+                oleDbCommand.Parameters.AddWithValue("@name", name);
+                oleDbCommand.Parameters.AddWithValue("@alertTime", alertTime);
+                oleDbCommand.Parameters.AddWithValue("@alertMonth", alertMonth);
+                oleDbCommand.Parameters.AddWithValue("@alertYear", alertYear);
+                oleDbCommand.Parameters.AddWithValue("@created", created);
+
+                // Open the connection, execute the query and close the connection
+                oleDbCommand.Connection.Open();
+                rowsAffected = oleDbCommand.ExecuteNonQuery();
+                oleDbCommand.Connection.Close();
+
+            }
+            if (rowsAffected > 0)
+            {
+                //MessageBox.Show("Alert is inserted");
+                LoadData();
+            }
+            else { MessageBox.Show("Error inserting"); }
+
+        }
+
+        public DataTable GetLastIdInt()
+        {
+            DataTable dataTable = new DataTable();
+            using (OleDbDataAdapter OleDbDbDataAdapter = new OleDbDataAdapter())
+            {
+                // Create the command and set its properties
+                OleDbDbDataAdapter.SelectCommand = new OleDbCommand();
+                OleDbDbDataAdapter.SelectCommand.Connection = new OleDbConnection(AlertConnection);
+                OleDbDbDataAdapter.SelectCommand.CommandType = CommandType.Text;
+                // Assign the SQL to the command object
+                OleDbDbDataAdapter.SelectCommand.CommandText = " SELECT COUNT(*) From [AlertData$]";
+                // Fill the datatable from adapter
+                OleDbDbDataAdapter.Fill(dataTable);
+            }
+            return dataTable;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbName.Items.Add("Recharge Internet");
+            cmbName.Items.Add("Deposit Savings");
+            cmbName.Items.Add("Booking Tickets");
+            cmbName.Items.Add("Room Rent");
+            cmbName.Items.Add("Interview Call");
+            cmbName.Items.Add("Others");
+
+            //Load the Xls sheet from google drive
+            bool fetchitfromgoogle = Utilities.RestoreItFromGoogleDrive(allfiles);
+            if (fetchitfromgoogle)
+            {
+                //MessageBox.Show("Updated in Google Drive");
+            }
+            else { MessageBox.Show("Error in fetching the data from google"); }
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+
+            //fetch all the data
+            DataTable dataTable = new DataTable();
+			
+
+            using (OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter())
+            {
+                // Create the command and set its properties
+                oleDbDataAdapter.SelectCommand = new OleDbCommand();
+                oleDbDataAdapter.SelectCommand.Connection = new OleDbConnection(this.AlertConnection);
+                oleDbDataAdapter.SelectCommand.CommandType = CommandType.Text;
+
+                oleDbDataAdapter.SelectCommand.CommandText = "Select Name, AlertTime, Created" +
+                " From [AlertData$] order by YEAR(AlertTime) DESC, MONTH(AlertTime) DESC, DAY(AlertTime)"; // Scripts.sqlGetAllExpense;
+
+                // Fill the datatable from adapter
+                oleDbDataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string name = cmbName.SelectedItem.ToString();
+            string updatedname = name.Replace(' ', '_');
+            LoadResults(updatedname);
+        }
+
+        private void LoadResults(string name)
+        {
+            DataTable dataTable = new DataTable();
+            using (OleDbDataAdapter OleDbDbDataAdapter = new OleDbDataAdapter())
+            {
+                // Create the command and set its properties
+                OleDbDbDataAdapter.SelectCommand = new OleDbCommand();
+                OleDbDbDataAdapter.SelectCommand.Connection = new OleDbConnection(AlertConnection);
+                OleDbDbDataAdapter.SelectCommand.CommandType = CommandType.Text;
+                // Assign the SQL to the command object
+                OleDbDbDataAdapter.SelectCommand.CommandText = "SELECT * From [AlertData$] where Name='" + name + "' order by Id";
+                // Fill the datatable from adapter
+                OleDbDbDataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void cmbName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string result = cmbName.SelectedItem.ToString();
+            if (result=="Others")
+            {
+                txtOthers.Enabled = true;
+            }
+            else { txtOthers.Enabled = false; }
+        }
+    }
+}
